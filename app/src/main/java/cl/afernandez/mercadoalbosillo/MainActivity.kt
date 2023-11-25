28207374460ab54e2c642caf1b84df530cffb9e6
Adolfo Fernandez <fantasmarobatortas5@gmail.com>
2023-11-25 15:27:26 -0300

    Casi imprementada la toolbar

--- app/src/main/java/cl/afernandez/mercadoalbosillo/MainActivity.kt
+++ app/src/main/java/cl/afernandez/mercadoalbosillo/MainActivity.kt
@@ -3,17 +3,28 @@ package cl.afernandez.mercadoalbosillo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        MyToolbar().show(this)
     }
 
-    fun goInventory(view: View){
-        val intentInventoryList = Intent(this, InventoryListActivity::class.java)
-        startActivity(intentInventoryList)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
-    
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.inventarioOpcion) {
+            //startActivity(Intent(this, ))
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
